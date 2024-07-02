320cd29c26b046159e22740b4df8ca7324d569f5
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-02 14:43:25 -0300

    create init binder to prevent empty strings on submit

--- src/main/java/com/tidz/springdemo/mvc/CustomerController.java
+++ src/main/java/com/tidz/springdemo/mvc/CustomerController.java
@@ -1,9 +1,12 @@
 package com.tidz.springdemo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -18,8 +21,16 @@ public class CustomerController {
 		return "customer-form";
 	}
 
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+
 	@PostMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+
 		if (bindingResult.hasErrors()) {
 			return "customer-form";
 		} else {
