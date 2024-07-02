88d0af8fae7db396f60090bddea23b323e39d40b
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-02 14:45:49 -0300

    add new attribute to Customer class and set its min and max values

--- src/main/java/com/tidz/springdemo/mvc/Customer.java
+++ src/main/java/com/tidz/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.tidz.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,9 @@ public class Customer {
 	@NotNull(message = "is required")
 	@Size(min = 1, message = "is required")
 	private String lastName = "";
+	@Min(value = 0, message = "must be greater than or equal to zero")
+	@Max(value = 10, message = "must be lesser than or equal to ten")
+	private int freePasses;
 
 	public Customer() {
 
@@ -28,6 +33,14 @@ public class Customer {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
 	}
 
 }
