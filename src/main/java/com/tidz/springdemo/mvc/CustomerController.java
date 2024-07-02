04a4b62489d8fad334040e2b12933b933fae97d4
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-02 11:28:37 -0300

    create CustomerController and add showForm end point

---
+++ src/main/java/com/tidz/springdemo/mvc/CustomerController.java
@@ -0,0 +1,15 @@
+package com.tidz.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomerController {
+
+	@GetMapping("/")
+	public String showForm(Model model) {
+		model.addAttribute("customer", new Customer());
+		return "customer-form";
+	}
+}
