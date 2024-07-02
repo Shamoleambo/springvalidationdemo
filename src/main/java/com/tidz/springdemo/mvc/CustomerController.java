b83af1a6581b75045c66b72c1a215435a0699e81
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-02 11:44:29 -0300

    add validation to the customer registration endpoin

--- src/main/java/com/tidz/springdemo/mvc/CustomerController.java
+++ src/main/java/com/tidz/springdemo/mvc/CustomerController.java
@@ -2,7 +2,12 @@ package com.tidz.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
@@ -11,5 +16,14 @@ public class CustomerController {
 	public String showForm(Model model) {
 		model.addAttribute("customer", new Customer());
 		return "customer-form";
+	}
+
+	@PostMapping("/processForm")
+	public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "customer-form";
+		} else {
+			return "customer-confirmation";
+		}
 	}
 }
