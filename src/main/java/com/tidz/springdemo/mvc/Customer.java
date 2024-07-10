122bdef15da8e42a9f2bbcaa5e06d4d9aafc0077
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-10 14:00:36 -0300

    Add postal code field to form

--- src/main/java/com/tidz/springdemo/mvc/Customer.java
+++ src/main/java/com/tidz/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.tidz.springdemo.mvc;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
@@ -14,6 +15,8 @@ public class Customer {
 	@Min(value = 0, message = "must be greater than or equal to zero")
 	@Max(value = 10, message = "must be lesser than or equal to ten")
 	private int freePasses;
+	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+	private String postalCode;
 
 	public Customer() {
 
@@ -41,6 +44,14 @@ public class Customer {
 
 	public void setFreePasses(int freePasses) {
 		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 
 }
