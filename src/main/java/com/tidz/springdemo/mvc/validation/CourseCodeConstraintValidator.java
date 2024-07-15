8c1d79db110add85e8f9a195377c0ea074c86e5c
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-15 10:23:51 -0300

    create CourseCode and CourseCodeConstraintValidator

---
+++ src/main/java/com/tidz/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,21 @@
+package com.tidz.springdemo.mvc.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+	private String coursePrefix;
+
+	@Override
+	public void initialize(CourseCode courseCode) {
+		this.coursePrefix = courseCode.value();
+	}
+
+	@Override
+	public boolean isValid(String code, ConstraintValidatorContext context) {
+		boolean result = code.startsWith(this.coursePrefix);
+		return result;
+	}
+
+}
